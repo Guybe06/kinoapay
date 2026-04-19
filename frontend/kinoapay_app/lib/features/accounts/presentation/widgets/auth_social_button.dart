df736ebac88b64ccd49d8d6a72ddb9a160578360
guybe06 <gmav94@gmail.com>
2026-04-19 11:58:08 +0100

    fix(accounts): supprimer import font_awesome inutilisé dans auth_social_button

--- frontend/kinoapay_app/lib/features/accounts/presentation/widgets/auth_social_button.dart
+++ frontend/kinoapay_app/lib/features/accounts/presentation/widgets/auth_social_button.dart
@@ -1,5 +1,4 @@
 import "package:flutter/material.dart";
-import "package:font_awesome_flutter/font_awesome_flutter.dart";
 import "package:kinoapay_app/core/constants/app_colors.dart";
 
 /// Bouton de connexion via un fournisseur externe (Google, Apple…).
